49e43abaca516571c03c6d3c9cfc41960974de4c
Lance Chaney <lance.e.chaney@gmail.com>
2019-10-03 21:49:29 +1300

    Added unsupervised model.

--- net.py
+++ net.py
@@ -20,3 +20,54 @@ class AlexNetPlusLatent(nn.Module):
         features = self.sigmoid(x)
         result = self.Linear2(features)
         return features, result
+
+class AutoencoderPlusLatent(nn.Module):
+    def __init__(self, bits):
+        super(AutoencoderPlusLatent, self).__init__()
+        self.bits = bits
+        self.features = nn.Sequential(*list(alexnet_model.features.children()))
+        self.remain = nn.Sequential(*list(alexnet_model.classifier.children())[:-1])
+        self.Linear1 = nn.Linear(4096, self.bits)
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU()
+        
+        # Decoder
+        self.DLinear1 = nn.Linear(self.bits, 4096)
+        self.DLinear2 = nn.Linear(4096, 9216)
+        self.decoder = nn.Sequential(
+            nn.Upsample(size=13),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Upsample(size=27),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Upsample(size=55),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 3, kernel_size=3, padding=1),
+            nn.ReLU(True),
+            nn.Upsample(size=227),
+            nn.Conv2d(3, 3, kernel_size=3, padding=1),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.remain(x)
+        x = self.Linear1(x)
+        features = self.sigmoid(x)
+        
+        # Decoder
+        d = self.DLinear1(features)
+        d = self.relu(d)
+        d = self.DLinear2(d)
+        d = self.relu(d)
+        d = d.view(d.size(0), 256, 6, 6)
+        result = self.decoder(d)
+
+        return features, result
---
+++ train-unsupervised.py
@@ -0,0 +1,126 @@
+import os
+import shutil
+import argparse
+
+import torch
+import torch.nn as nn
+
+from net import AlexNetPlusLatent, AutoencoderPlusLatent
+
+from torchvision import datasets, models, transforms
+from torchvision.utils import save_image
+from torch.autograd import Variable
+import torch.optim.lr_scheduler
+
+
+parser = argparse.ArgumentParser(description='Deep Hashing')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum (default: 0.9)')
+parser.add_argument('--epoch', type=int, default=128, metavar='epoch',
+                    help='epoch')
+parser.add_argument('--pretrained', type=int, default=0, metavar='pretrained_model',
+                    help='loading pretrained model(default = None)')
+parser.add_argument('--bits', type=int, default=48, metavar='bts',
+                    help='binary bits')
+parser.add_argument('--path', type=str, default='model_unsupervised', metavar='P',
+                    help='path directory')
+args = parser.parse_args()
+
+best_acc = 0
+start_epoch = 1
+transform_train = transforms.Compose(
+    [transforms.Resize(256),
+     transforms.RandomCrop(227),
+     transforms.RandomHorizontalFlip(),
+     transforms.ToTensor(),
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+transform_test = transforms.Compose(
+    [transforms.Resize(227),
+     transforms.ToTensor(),
+     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+trainset = datasets.CIFAR10(root='./data', train=True, download=True,
+                            transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                          shuffle=True, num_workers=2)
+
+testset = datasets.CIFAR10(root='./data', train=False, download=True,
+                           transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100,
+                                         shuffle=True, num_workers=2)
+
+net = AutoencoderPlusLatent(args.bits)
+
+use_cuda = torch.cuda.is_available()
+
+if use_cuda:
+    net.cuda()
+
+MSELoss = nn.MSELoss().cuda()
+
+optimizer4nn = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=0.0005)
+
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer4nn, milestones=[64], gamma=0.1)
+
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
+
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    for batch_idx, (inputs, _) in enumerate(trainloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), inputs.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        _, outputs = net(inputs)
+        loss = MSELoss(outputs, targets)
+        optimizer4nn.zero_grad()
+
+        loss.backward()
+
+        optimizer4nn.step()
+
+        train_loss += loss.item()
+
+        print(batch_idx, len(trainloader), 'Loss: %.3f'
+            % (train_loss/(batch_idx+1)))
+    return train_loss/(batch_idx+1)
+
+def test():
+    net.eval()
+    test_loss = 0
+    for batch_idx, (inputs, _) in enumerate(testloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), inputs.cuda()
+        inputs, targets = Variable(inputs), Variable(targets)
+        _, outputs = net(inputs)
+        loss = MSELoss(outputs, targets)
+        test_loss += loss.item()
+
+        print(batch_idx, len(testloader), 'Loss: %.3f'
+            % (test_loss/(batch_idx+1)))
+    if epoch == args.epoch:
+        print('Saving')
+        if not os.path.isdir('{}'.format(args.path)):
+            os.mkdir('{}'.format(args.path))
+        torch.save(net.state_dict(), './{}/{}'.format(args.path, test_loss/(batch_idx+1)))
+        pic = to_img(outputs.cpu().data)
+        save_image(pic, './dc_img/image_{}.png'.format(epoch))
+
+if __name__ == '__main__':
+    torch.multiprocessing.freeze_support()
+    if args.pretrained:
+        net.load_state_dict(torch.load('./{}/{}'.format(args.path, args.pretrained)))
+        test()
+    else:
+        if os.path.isdir('{}'.format(args.path)):
+            shutil.rmtree('{}'.format(args.path))
+        for epoch in range(start_epoch, start_epoch+args.epoch):
+            train(epoch)
+            test()
+            scheduler.step()
